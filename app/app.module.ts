848ab893e8c782bcfc50c6dd2a5307aa41b8695e
Srikar Reddy <srikar.nr@gmail.com>
2016-12-23 20:21:25 +0100

    Added a search form and linke it to a service to fetch Artist data from Spotify API

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { routing } from './app.routes'
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { routing } from './app.routes';
 
 import { AppComponent }  from './app.component';
 import {AboutComponent} from "./components/about/about.component";
@@ -9,7 +11,7 @@ import {SearchComponent} from "./components/search/search.component";
 import {NavbarComponent} from "./components/navbar/navbar.component";
 
 @NgModule({
-  imports:      [ BrowserModule, routing ],
+  imports:      [ BrowserModule, FormsModule, routing, HttpModule],
   declarations: [ AppComponent, AboutComponent, CatalogComponent, SearchComponent, NavbarComponent ],
   bootstrap:    [ AppComponent ]
 })
--- app/components/search/search.component.ts
+++ app/components/search/search.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'search',
-  template: `<h1>Hello {{name}}</h1>`,
-})
-export class SearchComponent  { name = 'Search Component'; }
 /**
  * Created by Srikar on 22-12-2016.
  */
+import { Component } from '@angular/core';
+import {SpotifyService} from "../../services/spotify.service";
+
+@Component({
+  moduleId: module.id,
+  selector: 'search',
+  templateUrl: 'search.component.html',
+  providers: [SpotifyService]
+  })
+
+export class SearchComponent  {
+  searchStr: string;
+  setArtists: artist[];
+
+  constructor(private _spotifyService: SpotifyService){
+  }
+
+  searchArtist(){
+    this._spotifyService.searchMusic(this.searchStr)
+        .subscribe( res => {
+          this.setArtists = res.artists.items;
+        })
+  }
+}
+
+interface artist{
+  id: number;
+  name: string;
+  genres: any;
+  albums: [
+    {
+      id: number;
+    }
+  ]
+}
+
